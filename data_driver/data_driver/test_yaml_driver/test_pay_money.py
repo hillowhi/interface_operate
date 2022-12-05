19093fd4df196c8a9f2091be9d52f50db4ff1938
hh <957511968@qq.com>
2022-12-05 19:21:00 +0800

    付款&下载重试机制

--- data_driver/data_driver/test_yaml_driver/test_pay_money.py
+++ data_driver/data_driver/test_yaml_driver/test_pay_money.py
@@ -1,6 +1,11 @@
-from data_driver.data_driver.yaml_driver.book_pay_process import Book_Pay_Process
+
+
+'''
+充值
+'''
+from data_driver.data_driver.yaml_driver.book_pay_process import BookPayProcess
 
 
 def test_pay_money(get_browser):
-    book_pay_process = Book_Pay_Process(get_browser)
+    book_pay_process = BookPayProcess(get_browser)
     book_pay_process.book_pay_process()
--- data_driver/data_driver/yaml_driver/book_pay_process.py
+++ data_driver/data_driver/yaml_driver/book_pay_process.py
@@ -1,16 +1,31 @@
-from time import sleep
+'''
+登录&点击充值
+'''
 
+from time import sleep
 from data_driver.data_driver.key_word_class.key_operate_browser import KeyOperateBrowser
 from data_driver.data_driver.yaml_data import KEY_WORD
 from data_driver.data_driver.yaml_driver.login_process import Login
 
 
-class Book_Pay_Process(KeyOperateBrowser):
+class BookPayProcess(KeyOperateBrowser):
     def book_pay_process(self):
-        # 调用登录流程
+        # 调用登录流程 普通版
+        # login_pay = Login(self.driver)
+        # login_pay.url_login(KEY_WORD.YAML_LOGIN, KEY_WORD.PAY_USERNAME, KEY_WORD.PAY_PASSWORD)
+        # self.wait_locat(*KEY_WORD.PAY_BUTTON).click()
+        # sleep(2)
+        # self.wait_locat(*KEY_WORD.PAY_MONEY).click()
+        # sleep(3)
+        # 当请求遇到异常怎么办 走重试机制 下面重试机制是重试多少次
+
+        # 登录
         login_pay = Login(self.driver)
         login_pay.url_login(KEY_WORD.YAML_LOGIN, KEY_WORD.PAY_USERNAME, KEY_WORD.PAY_PASSWORD)
-        self.wait_locat(*KEY_WORD.PAY_BUTTON).click()
-        sleep(2)
-        self.wait_locat(*KEY_WORD.PAY_MONEY).click()
-        sleep(3)
+        # 跳转到付款页 如果页面出现请求异常 就一直请求
+        while True:
+            try:
+                self.wait_locat(*KEY_WORD.PAY_BUTTON).click()
+                break
+            except:
+                pass
